9e0a96d7aa13d3a3f40738b7feb930fa29620fbb
manicpixiecat <tatyibnida@gmail.com>
2025-04-03 14:21:10 +0000

    criando váriaveis e classes com scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+ String nomeCliente = args [0];
+String numeroAgencia = args [1];
+int numeroConta = Integer.valueOf() [3]
+double Saldo = 237.48;
+}
 }
