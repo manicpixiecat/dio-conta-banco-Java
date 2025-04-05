57552b40a9830e3443b9cb446898e55a6beccf8e
manicpixiecat <tatyibnida@gmail.com>
2025-04-05 03:21:16 +0000

    finalizado simulação conta banco via terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,25 @@
-public class ContaTerminal
-    public static void main(String[] args) throws Exception {
- String nomeCliente = args [0];
-String numeroAgencia = args [1];
-int numeroConta = Integer.valueOf() [3]
-double Saldo = 237.48;
-}
+import Sjava.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double saldo = 237.48;
+        
+    System.out.println("Digite seu nome completo");
+    String nomeCliente = scanner.next();
+    System.out.println("Digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.next();
+        
+        System.out.println("Digite sua Agência");
+        String numeroAgencia = scanner.next();
+        
+        System.out.print("Olá " + nomeCliente);
+        System.out.print(", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia);
+        System.out.print(", conta " + numeroConta);
+        System.out.print(" e seu saldo " + saldo);
+        System.out.println(" já está disponível para saque.");
+        
+    }
 }
